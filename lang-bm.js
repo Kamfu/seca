0bcb7789c903f54a364e202ea1232501a43ea585
kamfu-charlie <33866278+kamfu-charlie@users.noreply.github.com>
2018-12-26 12:06:22 +0800

    Others files

---
+++ lang-bm.js
@@ -0,0 +1,184 @@
+var dict = {
+"{new}": {
+  en: "",
+  bm: "",
+  cn: "",
+},
+"{lang_switcher}": {
+  en: "Malaysia",
+  bm: "English",
+  cn: "中文",
+},
+"{lang}": {
+  en: "LANGUAGE",
+  bm: "BAHASA",
+  cn: "语言",
+},
+"{register}": {
+  en: "Register",
+  bm: "Daftar",
+  cn: "注册",
+},
+"{login}": {
+  en: "Login",
+  bm: "Log Masuk",
+  cn: "登入",
+},
+"{intro}": {
+  en: "INTRODUCTION",
+  bm: "PENGENALAN",
+  cn: "公会简介",
+},
+"{contact}": {
+  en: "CONTACT US",
+  bm: "HUBUNGI KAMI",
+  cn: "联络我们",
+},
+"{news}": {
+  en: "NEWS",
+  bm: "BERITA",
+  cn: "活动新闻",
+},
+"{lists}": {
+  en: "MEMBERS",
+  bm: "AHLI PERSATUAN",
+  cn: "公会会员",
+},
+// Footer Navigation
+"{faq}": {
+  en: "FAQ",
+  bm: "Soalan Lazim",
+  cn: "知识库",
+},
+"{tnc}": {
+  en: "Terms",
+  bm: "Terma",
+  cn: "使用条款",
+},
+"{privacy}": {
+  en: "Privacy",
+  bm: "Privasi",
+  cn: "个人隐私",
+},
+"{copyrights}": {
+  en: "Copyrights",
+  bm: "Hak Cipta",
+  cn: "版权所有",
+},
+// UI Component
+"{badge_new}": {
+  en: "NEW",
+  bm: "BARU",
+  cn: "最新",
+},
+"{badge_hot}": {
+  en: "HOT",
+  bm: "HANGAT",
+  cn: "热门",
+},
+
+"{stories}": {
+  en: "Successfully Stories",
+  bm: "Kisah yang Berjaya",
+  cn: "成功故事",
+},
+"{shop}": {
+  en: "Catalogue",
+  bm: "Katalog",
+  cn: "商品目录",
+},
+"{sponsors}": {
+  en: "Sponsors",
+  bm: "Penaja",
+  cn: "赞助商",
+},
+"{courses_online}": {
+  en: "Online Courses",
+  bm: "Kursus Dalam Talian",
+  cn: "线上课程",
+},
+
+"{courses_offline}": {
+  en: "Offline Courses",
+  bm: "Kursus Luar Talian",
+  cn: "线下课程",
+},
+"{education}": {
+  en: "Education",
+  bm: "",
+  cn: "教育",
+},
+"{exhibition}": {
+  en: "Exhibition",
+  bm: "",
+  cn: "展览会",
+},
+"{seminar}": {
+  en: "Seminar",
+  bm: "",
+  cn: "交流会",
+},
+"{seminar}": {
+  en: "Seminar",
+  bm: "",
+  cn: "交流会",
+},
+"{kuching}": {
+  en: "Kuching",
+  bm: "",
+  cn: "古晋",
+},
+"{sibu}": {
+  en: "Sibu",
+  bm: "",
+  cn: "诗巫",
+},
+"{bintulu}": {
+  en: "Bintulu",
+  bm: "",
+  cn: "民都鲁",
+},
+"{miri}": {
+  en: "Miri",
+  bm: "",
+  cn: "美里",
+},
+
+
+"{page3_button}": {
+  en: "VIEW DETAILS",
+  bm: "BUTIRAN",
+},
+
+"{page3_label_time}": {
+  en: "Time :",
+  bm: "Masa :",
+},
+
+"{page3_label_date}": {
+  en: "Date :",
+  bm: "Tarikh :",
+},
+
+"{page3_label_location}": {
+  en: "Location :",
+  bm: "Lokasi :",
+},
+
+"{tutorial_button2_label}": {
+  en: "Express Sign Up",
+  bm: "Pendaftaran Ekspres",
+},
+
+"{tutorial_ticket_label}": {
+  en: "Make Payment",
+  bm: "Bayar",
+},
+
+"{tutorial_email_label}": {
+  en: "Email Activation",
+  bm: "Emel Pengesahan",
+},
+
+
+}// END of tran
